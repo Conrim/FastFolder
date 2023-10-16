3e17ddb4fefa8be02341dc3ff3a591494d428dd1
Konrad <konrad.rimmele@gmail.com>
2023-10-16 14:20:24 +0200

    add existing project

---
+++ FolderCreater/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using File = System.IO.File;
+using IWshRuntimeLibrary;
+
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace FolderCreater
+{
+    public partial class MainWindow : Window
+    {
+        private string parentFolderPath = Directory.GetParent(Assembly.GetEntryAssembly().Location).FullName;
+        private string folderOpenerPath; // = Path.Combine(parentFolderPath, "OpenFolder.exe")
+        public MainWindow()
+        {
+            folderOpenerPath = @"K:\FastFolder\FolderOpener\bin\Release\FolderOpener.exe"; //Path.Combine(parentFolderPath, "OpenFolder.exe");
+            InitializeComponent();
+            CwdTextBox.Text = Directory.GetCurrentDirectory();
+        }
+        private string GetFreePath(string wantedPath) => GetFreePath(wantedPath, "");
+        private string GetFreePath(string wantedPath, string extension)
+        {
+            string path = wantedPath + extension;
+            int i = 1;
+            while (Directory.Exists(path) || File.Exists(path))
+            {
+                path = wantedPath + i.ToString() + extension;
+                i++;
+            }
+            return path;
+        }
+        void Cancel(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        void OnCreateFolderAndShortcut(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string folderPath = CreateFolderAndShortcut();
+                MsgBox.Text = $"Ordner: '{Path.GetFileName(folderPath)}' erfolgreich erstellt";
+            }
+            catch (Exception exception)
+            {
+                MsgBox.Text = exception.Message;
+            }
+        }
+        string CreateFolderAndShortcut()
+        {
+            // Error Checking
+            Regex re = new Regex("[" + Regex.Escape(new string(Path.GetInvalidFileNameChars())) + "]"); //Stackoverflow Code
+            if (re.IsMatch(NameTextBox.Text)) throw new Exception($"Der Dateiname: '{NameTextBox.Text}' enthält illegale Zeichen");
+            if (!Directory.Exists(CwdTextBox.Text)) throw new Exception($"Der angegeben Pfad: '{CwdTextBox.Text}' existiert nicht");
+
+            string lnkFile = GetFreePath(Path.Combine(CwdTextBox.Text, NameTextBox.Text), ".lnk");
+            string folderPath = GetFreePath(Path.Combine(parentFolderPath, @"data\" + Path.GetFileNameWithoutExtension(lnkFile)));
+
+
+            // creates Folder
+            Directory.CreateDirectory(folderPath);
+
+            // creates Shortcut
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = shell.CreateShortcut(lnkFile);
+            shortcut.WorkingDirectory = folderPath;
+            shortcut.TargetPath = folderOpenerPath;
+            shortcut.Save();
+
+            return folderPath;
+        }
+    }
+}
